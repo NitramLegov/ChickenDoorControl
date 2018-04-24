9676ad21dba4287b110c11eae51c19581f1520bb
NitramLegov <Martin_Vogel@live.com>
2018-04-24 14:55:04 +0200

    Removed apt-get update from Install.sh

--- ChickenDoorControl/Install.sh
+++ ChickenDoorControl/Install.sh
@@ -46,8 +46,9 @@ CONFIG=/boot/config.txt
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 echo 'Welcome to the Chicken door control setup'
 echo 'Please note that the initial setup requires an active internet connection.'
-echo 'First, we will update the apt-get database'
-sudo apt-get -qq update
+#So far, nothing is installed using apt-get. So refreshing the db should not be needed right now.
+#echo 'First, we will update the apt-get database'
+#sudo apt-get -qq update
 echo '----------------'
 echo 'Installing python prerequisites for the Chicken door control Software: web.py and pyephem'
 sudo pip install web.py pyephem
